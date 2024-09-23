fd13430f50608580f0ea7b23e377be1ebba241b6
Oleksii Aleshchenko <oleksii.aleshchenko@playtomax.com>
2024-09-23 04:13:06 +0300

    add resizes // refactor

--- src/Game.js
+++ src/Game.js
@@ -3,17 +3,38 @@
  */
 class Game {
     static {
-        Game.init().then(() => console.log('Game initialized', Game.app.stage.children));
+        this.init().then(() => console.log('Game initialized', this.app.stage.children));
     }
 
     static async init() {
-        const app = new PIXI.Application();
-        await app.init({ background: '#1099bb', resizeTo: window });
-        document.body.appendChild(app.canvas);
+        this.app = new PIXI.Application();
+        await this.app.init({ background: '#1099bb', resizeTo: window });
+        document.body.appendChild(this.app.canvas);
 
-        await Game.loadAssets();
+        await this.loadAssets();
+        this.initChildren();
+        this.initListeners();
+    }
 
-        Game.symbolsTextures = [
+    static initListeners() {
+        window.addEventListener('resize', () => {
+            // this.app.renderer.resize(window.innerWidth, window.innerHeight);
+            this.onResize();
+        });
+    }
+
+    static onResize() {
+        this.initFieldScale();
+        this.initFieldPosition();
+    }
+
+    static initChildren() {
+        this.initSymbolsTextures();
+        this.initField();
+    }
+
+    static initSymbolsTextures() {
+        this.symbolsTextures = [
             PIXI.Texture.from('assets/light_rotate_1.png'),
             PIXI.Texture.from('assets/light_rotate_2.png'),
             PIXI.Texture.from('assets/rt_object_01.png'),
@@ -25,10 +46,28 @@ class Game {
             PIXI.Texture.from('assets/rt_object_07.png'),
             PIXI.Texture.from('assets/rt_object_08.png'),
         ];
+    }
 
-        app.stage.addChild(new Field());
+    static initField() {
+        this.field = new Field();
+        this.initFieldScale();
+        this.initFieldPosition();
+        this.app.stage.addChild(this.field);
+    }
 
-        Game.app = app;
+    static initFieldScale() {
+        const scale = Math.min(this.app.screen.width / CONFIG.ROLL_WIDTH / CONFIG.ROLLS_QUANTITY,
+            this.app.screen.height / CONFIG.SYMBOL_SIZE / CONFIG.ROWS_QUANTITY);
+        this.field.scale.set(scale);
+    }
+
+    static initFieldPosition() {
+        // todo verticalMargin needed?
+        // const verticalMargin = (this.app.screen.height - CONFIG.SYMBOL_SIZE * CONFIG.ROWS_QUANTITY) / 2;
+        const verticalMargin = (this.app.screen.height - this.field.height) / 2;
+        this.field.y = verticalMargin;
+        // this.field.x = Math.round((this.app.screen.width - CONFIG.ROLL_WIDTH * CONFIG.ROLLS_QUANTITY) / 2);
+        this.field.x = Math.round((this.app.screen.width - this.field.width) / 2);
     }
 
     /**
